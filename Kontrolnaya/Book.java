d58d0e79f24ee0f961583f7c057527387fd9bd88
IzvanovMV <114096423+IzvanovMV@users.noreply.github.com>
2022-11-05 13:29:21 +0300

    Create Book.java

---
+++ Kontrolnaya/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    private String name;
+    private String author;
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
