76976f71d8ce879a16637346e8c10b0b5bebf62a
IzvanovMV <114096423+IzvanovMV@users.noreply.github.com>
2023-04-15 00:58:53 +0300

    Delete toDoList directory

--- toDoList/src/main/java/com/List/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.List.repositories;
-import com.List.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
-
